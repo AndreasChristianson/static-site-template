5e9d8fa8178363cd0a1e99f2aa0a6d1e68bb0731
Andreas Christianson <sullage@pessimistic-it.com>
2020-11-16 02:02:44 -0600

    initial deployment

--- src/pages/Homepage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styled from 'styled-components'
-
-const PageContainer = styled.div``
-
-export const Homepage = (props) => 
-  <PageContainer>
-    <h1>Hello world</h1>
-  </PageContainer>
