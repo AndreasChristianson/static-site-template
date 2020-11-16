97a8fe62e57014112d079845c2ca4518ec418ab3
Andreas Christianson <sullage@pessimistic-it.com>
2020-11-16 00:44:40 -0600

    webpack+babel+react

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'devicon/fonts/devicon.eot'
+import 'fontsource-merriweather'
+import 'fontsource-open-sans'
+import { Homepage } from './pages/Homepage';
+
+
+ReactDOM.render(
+  <Homepage />,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+process.traceDeprecation = true;
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'app.js'),
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+      inject: 'body'
+  }),
+  ],
+  optimization: {
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  }
+};
